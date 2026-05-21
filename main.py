9589258776258ec8e4a06d12e3fd44e0e0441b2c
jjforsyth15 <jjforsyth15@gmail.com>
2026-05-20 21:27:12 -0700

    Drawing circuits

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from qiskit import QuantumCircuit
 from qiskit.primitives import StatevectorSampler
+import matplotlib.pyplot as plt
 
-qc = QuantumCircuit(1)
+qc = QuantumCircuit(2)
 
 qc.h(0)
+qc.cx(0, 1)
 
 qc.measure_all()
+
+print(qc) 
 
 sampler = StatevectorSampler()
 result = sampler.run([qc]).result()
 
-print(qc)
 print(result)
 
 qc.draw("mpl")
+plt.show()
